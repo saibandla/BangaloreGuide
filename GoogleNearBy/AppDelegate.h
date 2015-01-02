0865b30ea79c92146302548ccd06e24073f8f38f
Geniusport <adeptpros@Adeptpross-Mac-mini-10.local>
2015-01-02 14:39:02 +0530

    Initial Commit

---
+++ GoogleNearBy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoogleNearBy
+//
+//  Created by Geniusport on 1/2/15.
+//  Copyright (c) 2015 Adeptpros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoogleNearBy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoogleNearBy
+//
+//  Created by Geniusport on 1/2/15.
+//  Copyright (c) 2015 Adeptpros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
